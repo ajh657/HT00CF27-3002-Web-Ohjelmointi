e42b102da3af2a70ac324da23835010bf098e041
Arttu Höylä <aahoo01@gmail.com>
2026-03-20 11:12:29 +0200

    TH2

---
+++ Svelte/src/routes/laskin/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let x: number = $state(0);
+	let y: number = $state(0);
+
+	function lisaaX(): void {
+		x++;
+	}
+	function lisaaY(): void {
+		y++;
+	}
+</script>
+
+<h1>{x} * {y} = {x * y}</h1>
+
+<button onclick={lisaaX}> X++ </button>
+<button onclick={lisaaY}> X++ </button>
