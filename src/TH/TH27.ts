cb71012e724c33e9ecf72c57433ed092395b32b3
Arttu Höylä <aahoo01@gmail.com>
2026-03-20 10:11:36 +0200

    27 Completion

--- src/TH/TH27.ts
+++ src/TH/TH27.ts
@@ -1,6 +1,10 @@
 class Laatikko<T> {
   private sisalto: T | null = null;
 
+  public constructor(tavara: T | null) {
+    this.sisalto = tavara;
+  }
+
   public Laita(x: T): void {
     this.sisalto = x;
   }
@@ -10,4 +14,16 @@ class Laatikko<T> {
     this.sisalto = null;
     return tavara;
   }
+
+  public katoSisalto(): T | null {
+    return this.sisalto;
+  }
 }
+
+const laatikko = new Laatikko<string>('arguments');
+
+console.log(laatikko.katoSisalto());
+
+laatikko.Laita('testi');
+console.log(laatikko.ota());
+console.log(laatikko.katoSisalto());
